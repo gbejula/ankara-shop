ee42e768cf56b9ea52de91ca61dce1dc9c2a59fc
Olugbenga Ajayi <gbejula@gmail.com>
2022-01-21 22:10:44 +0100

    creating login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <Router>
+    <Routes>
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomePage} exact />
-          <Route path='/product/:id' component={ProductPage} />
-          <Route path='/cart/:id?' component={CartPage} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/product/:id' element={<ProductPage />} />
+          <Route path='/cart/:id?' element={<CartPage />} />
+          <Route path='/' element={<HomePage />} />
         </Container>
       </main>
       <Footer />
-    </Router>
+    </Routes>
   );
 }
 
