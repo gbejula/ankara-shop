a2b17d962d1d841d5b27f08ecef4d4f2ef15ac2e
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-11 23:12:18 +0100

    starting shipping page

---
+++ frontend/src/pages/ShippingPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+
+const ShippingPage = () => {
+  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState('');
+  return <div>ShippingPage</div>;
+};
+
+export default ShippingPage;
