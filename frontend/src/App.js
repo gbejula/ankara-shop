3254e1acef886cadefc41681d53839b9122ade1d
Olugbenga Ajayi <gbejula@gmail.com>
2022-03-18 07:31:25 +0100

    added shipping page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
-// import ShippingPage from './pages/ShippingPage';
+import ShippingPage from './pages/ShippingPage';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          {/* <Route path='/shipping' component={ShippingPage} /> */}
+          <Route path='/shipping' component={ShippingPage} />
           <Route path='/profile' component={ProfilePage} />
           <Route path='/register' component={RegisterPage} />
           <Route path='/signin' component={LoginPage} />
